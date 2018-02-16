2dcce2475298d394477bf4652b91ffe26555e286
stela <stella.plamenova@gmail.com>
2018-02-16 15:36:16 +0100

    typo fixed

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ var config = {
 	// intro view
 	"intro": {
 		// introduction title
-		"title": "Welcome to ExPrag experiments!",
+		"title": "Welcome to XPrag experiments!",
 		// introduction text
 		"text": "Lorem ipsum dolor sit amet consectetur adipiscing elit tellus auctor, risus metus mauris nibh leo senectus varius taciti bibendum, laoreet tempor orci ligula iaculis odio malesuada nostra. Erat gravida consequat nunc pharetra libero tempus lobortis placerat, laoreet vitae eget vivamus eros luctus sed nullam auctor, vel sem hac quam facilisis aptent blandit. Bibendum molestie morbi ullamcorper vitae accumsan dapibus ultricies aliquet mi luctus, ante suscipit purus consequat nascetur a senectus cras donec.",
 		// instroduction's slide proceeding button
